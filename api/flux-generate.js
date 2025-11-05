9da6c776d5076adbc8004d3377ddbf8f794a8724
bamkikovic <33391053+bamkikovic@users.noreply.github.com>
2025-11-05 16:37:51 +0100

    Add files via upload

---
+++ api/flux-generate.js
@@ -0,0 +1,16 @@
+{
+  "name": "nanobananascript",
+  "version": "1.0.0",
+  "description": "Secure proxy for OpenRouter API calls.",
+  "main": "api/flux-generate.js",
+  "scripts": {
+    "start": "node proxy_server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "node-fetch": "^2.7.0"
+  },
+  "devDependencies": {
+    "dotenv": "^16.4.5"
+  }
+}
